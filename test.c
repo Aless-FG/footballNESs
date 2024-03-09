a75b15f1fa6598b389b841e69e8dc0fb9f9160e2
Aless-FG <alessandroricchetti@protonmail.com>
2024-03-09 16:12:27 +0000

    added controller(s) input and L/R animations

--- test.c
+++ test.c
@@ -1,10 +1,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-
-#include <stdlib.h>
-#include <string.h>
-
 // include NESLIB header
 #include "neslib.h"
 
@@ -24,16 +20,51 @@
 #define NES_MIRRORING 0
 #define NUM_ACTORS 2
 
-#define TILE 0xd8
-#define ATTR 0x01
-
 // define a 2x2 metasprite
-const unsigned char metasprite[]={
-        0,      0,      TILE+0,   ATTR, 
-        0,      8,      TILE+1,   ATTR, 
-        8,      0,      TILE+2,   ATTR, 
-        8,      8,      TILE+3,   ATTR, 
+#define DEF_METASPRITE_2x2(name,code,pal)\
+const unsigned char name[]={\
+        0,      0,      (code)+0,   pal, \
+        0,      8,      (code)+1,   pal, \
+        8,      0,      (code)+2,   pal, \
+        8,      8,      (code)+3,   pal, \
         128};
+
+// define a 2x2 metasprite, flipped horizontally
+#define DEF_METASPRITE_2x2_FLIP(name,code,pal)\
+const unsigned char name[]={\
+        8,      0,      (code)+0,   (pal)|OAM_FLIP_H, \
+        8,      8,      (code)+1,   (pal)|OAM_FLIP_H, \
+        0,      0,      (code)+2,   (pal)|OAM_FLIP_H, \
+        0,      8,      (code)+3,   (pal)|OAM_FLIP_H, \
+        128};
+
+
+DEF_METASPRITE_2x2(playerRStand, 0xd8, 0);
+DEF_METASPRITE_2x2(playerRRun1, 0xdc, 0);
+DEF_METASPRITE_2x2(playerRRun2, 0xe0, 0);
+DEF_METASPRITE_2x2(playerRRun3, 0xe4, 0);
+DEF_METASPRITE_2x2(playerRJump, 0xe8, 0);
+DEF_METASPRITE_2x2(playerRClimb, 0xec, 0);
+DEF_METASPRITE_2x2(playerRSad, 0xf0, 0);
+
+DEF_METASPRITE_2x2_FLIP(playerLStand, 0xd8, 0);
+DEF_METASPRITE_2x2_FLIP(playerLRun1, 0xdc, 0);
+DEF_METASPRITE_2x2_FLIP(playerLRun2, 0xe0, 0);
+DEF_METASPRITE_2x2_FLIP(playerLRun3, 0xe4, 0);
+DEF_METASPRITE_2x2_FLIP(playerLJump, 0xe8, 0);
+DEF_METASPRITE_2x2_FLIP(playerLClimb, 0xec, 0);
+DEF_METASPRITE_2x2_FLIP(playerLSad, 0xf0, 0);
+
+DEF_METASPRITE_2x2(personToSave, 0xba, 1);
+
+const unsigned char* const playerRunSeq[16] = {
+  playerLRun1, playerLRun2, playerLRun3, 
+  playerLRun1, playerLRun2, playerLRun3, 
+  playerLRun1, playerLRun2,
+  playerRRun1, playerRRun2, playerRRun3, 
+  playerRRun1, playerRRun2, playerRRun3, 
+  playerRRun1, playerRRun2,
+};
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
@@ -89,8 +120,53 @@ void scroll_demo() {
   }
 }
 
+void movement(char i, char oam_id, char pad) { 
+  oam_id = 0; 
+  
+  for (i=0; i<2; i++) {
+    // poll controller i (0-1)
+    pad = pad_trigger(i); // takes several scanlines to complete
+    pad = pad_state(i);
+ // returns previous value
+    // move actor[i] left/right
+    if (pad&PAD_LEFT && actor_x[i]>0) {
+    	actor_dx[i]=-2;
+      	if(pad&PAD_B && actor_x[i]>0) // press B to run faster
+          actor_dx[i]=-4;
+      	
+    }
+    
+      	
+    else if (pad&PAD_RIGHT && actor_x[i]<240) {
+    	actor_dx[i]=2;
+      	if(pad&PAD_B && actor_x[i]<240) // press B to run faster
+          actor_dx[i]=4;
+    }
+      	
+    else 
+      	actor_dx[i]=0;
+    // move actor[i] up/down
+    if (pad&PAD_UP && actor_y[i]>0) 
+      	actor_dy[i]=-2;
+    else if (pad&PAD_DOWN && actor_y[i]<212)
+      	actor_dy[i]=2;
+    else 
+      	actor_dy[i]=0;
+  }
+  for (i=0; i<NUM_ACTORS; i++) {
+    byte runseq = actor_x[i] & 7;
+      if (actor_dx[i] >= 0)
+        runseq += 8;
+    oam_id = oam_meta_spr(actor_x[i], actor_y[i], oam_id, playerRunSeq[runseq]);
+    actor_x[i] += actor_dx[i];
+    actor_y[i] += actor_dy[i];
+  }
+
+  if (oam_id!=0) oam_hide_rest(oam_id);
+}
+
 void draw_pitch() {
-    int i,y;
+  int i,y;
   y = 2;
   setup_graphics();
    vram_adr(NTADR_A(2,2));
@@ -156,47 +232,33 @@ void draw_pitch() {
 
 void main(void)
 {
+  
+  //i = 0;
   char i;
   char oam_id;
-  //i = 0;
-  
+  char pad;
   draw_pitch();
   // enable rendering
   actor_x[0] = 50;
   actor_y[0] = 50;
   
   actor_dx[0] = 0;
-  actor_dy[0] = 1;
+  actor_dy[0] = 0;
   
   actor_x[1] = 90;
   actor_y[1] = 70;
   
-  actor_dx[1] = 1;
-  actor_dy[1] = 1;
-  
-  //scroll_demo();
-  
-  
+  actor_dx[1] = 0;
+  actor_dy[1] = 0;
   
   // infinite loop
   while(1) {
-    oam_id = 0;
-    
-    oam_id = oam_spr(actor_x[i], actor_y[i], 0xAF, 0x00, oam_id);
-    
-    actor_x[i] += actor_dx[i];
-    actor_y[i] += actor_dy[i];
-    oam_id = oam_meta_spr(actor_x[i+1], actor_y[i+1], oam_id, metasprite);
-    actor_x[i+1] += actor_dx[i+1];
-    actor_y[i+1] += actor_dy[i+1];
+    movement(i, oam_id, pad);
+  //scroll_demo();
     
     
     
     
-    
-    
-    
-    if (oam_id!=0) oam_hide_rest(oam_id);
     ppu_wait_frame();
   }
 }
